5a5538412f46f99ca3a1dbe3184ea7ca84422dcd
Arthur Fondeville <arthur.fondevillepro@gmail.com>
2026-01-07 13:46:11 +0100

    Initial Commit

---
+++ ProjectAlpha/ProjectAlphaApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProjectAlphaApp.swift
+//  ProjectAlpha
+//
+//  Created by Arthur Fondeville on 07/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct ProjectAlphaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ProjectAlphaTests/ProjectAlphaTests.swift
@@ -0,0 +1,17 @@
+//
+//  ProjectAlphaTests.swift
+//  ProjectAlphaTests
+//
+//  Created by Arthur Fondeville on 07/01/2026.
+//
+
+import Testing
+@testable import ProjectAlpha
+
+struct ProjectAlphaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
